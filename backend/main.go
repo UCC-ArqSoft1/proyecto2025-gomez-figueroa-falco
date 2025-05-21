9318af70bc75dcc573e163a41321a3902c55e02f
Franciscafalco <franciscafalco4@gmail.com>
2025-05-21 16:18:30 -0300

    se agregó la autorización del token

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,9 @@ import (
 	"backend/controllers"
 
 	"github.com/gin-contrib/cors"
+
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +14,15 @@ func main() {
 
 	router := gin.Default()
 	router.Use(cors.Default())
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	router.GET("/actividades", controllers.GetActividades)
 	router.GET("/actividad/:id", controllers.GetActividadById)
