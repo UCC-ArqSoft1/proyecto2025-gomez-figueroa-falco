b3e81782d73f4b69313a93e3ed13637b92203f43
Franciscafalco <franciscafalco4@gmail.com>
2025-05-30 15:28:43 -0300

    boton de inscribirse

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
                 setError("");
 
                 // Redirecciona, muestra mensaje o cambia de vista...
-                navigate("/Actividades");
+                navigate("/actividades");
             } else {
                 setError(data.error || "Error desconocido");
                 setSuccess("");
